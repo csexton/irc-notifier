a589978781bd70c31864057c210f3b8fdf79d247
Christopher Sexton <github@codeography.com>
2013-12-25 11:48:21 -0500

    Remove comment with semaphore example post

--- app.rb
+++ app.rb
@@ -2,24 +2,6 @@ require 'sinatra'
 require 'socket'
 require 'json'
 
-# {
-#   "branch_name": "gem_updates",
-#   "branch_url": "https://semaphoreapp.com/projects/44/branches/50",
-#   "project_name": "base-app",
-#   "build_url": "https://semaphoreapp.com/projects/44/branches/50/builds/15",
-#   "build_number": 15,
-#   "result": "passed",
-#   "started_at": "2012-07-09T15:23:53Z",
-#   "finished_at": "2012-07-09T15:30:16Z",
-#   "commit": {
-#     "id": "dc395381e650f3bac18457909880829fc20e34ba",
-#     "url": "https://github.com/renderedtext/base-app/commit/dc395381e650f3bac18457909880829fc20e34ba",
-#     "author_name": "Vladimir Saric",
-#     "author_email": "vladimir@renderedtext.com",
-#     "message": "Update 'shoulda' gem.",
-#     "timestamp": "2012-07-04T18:14:08Z"}
-# }
-
 def putss(s, str)
   s.puts str
   puts s
